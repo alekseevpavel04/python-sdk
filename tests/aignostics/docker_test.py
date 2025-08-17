34666527ea80efbcdcf29a40d2ad7ddd01154875
Helmut Hoffer von Ankershoffen <helmuthva@googlemail.com>
2025-08-17 10:49:57 +0200

    chore(native): rfc [build:native:only]

--- tests/aignostics/docker_test.py
+++ tests/aignostics/docker_test.py
@@ -18,8 +18,8 @@ BUILT_WITH_LOVE = "built with love in Berlin"
 @pytest.mark.skip_with_act
 @pytest.mark.xdist_group(name="docker")
 @pytest.mark.docker
-# @pytest.mark.long_running
-# @pytest.mark.scheduled
+@pytest.mark.long_running
+@pytest.mark.scheduled
 def test_core_docker_cli_help_with_love(docker_services) -> None:
     """Test the CLI help command with docker services returns expected output."""
     out = docker_services._docker_compose.execute("run aignostics --help")
