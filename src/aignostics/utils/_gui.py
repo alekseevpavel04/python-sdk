c7d0f466465dde1772d01c002e1fe245bd67a7e1
Helmut Hoffer von Ankershoffen <helmuthva@googlemail.com>
2025-08-17 20:32:54 +0200

    feat(native): linux in native window [build:native:only]

--- src/aignostics/utils/_gui.py
+++ src/aignostics/utils/_gui.py
@@ -70,9 +70,9 @@ def gui_run(  # noqa: PLR0913, PLR0917
         message = "with_api is not supported in this project."
         raise ValueError(message)
 
-    if native and platform.system() == "Linux":
-        native = False
-        show = True
+    # if native and platform.system() == "Linux":
+    #    native = False
+    #    show = True
 
     gui_register_pages()
 
