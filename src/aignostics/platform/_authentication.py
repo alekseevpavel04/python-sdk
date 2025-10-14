acd79027128a1ac788cbf514b3a25126dd297252
Helmut Hoffer von Ankershoffen <helmuthva@googlemail.com>
2025-10-14 12:34:49 +0200

    chore: Comment out print

--- src/aignostics/platform/_authentication.py
+++ src/aignostics/platform/_authentication.py
@@ -401,7 +401,9 @@ def _perform_authorization_code_with_pkce_flow() -> str:
                 token = session.fetch_token(settings().token_url, code=auth_code, include_client_id=True)
                 # Store the token
                 authentication_result.token = token["access_token"]
-                print(token["access_token"])
+                # TODO(Andreas): Commented this out, as it breaks tests and you don't want this
+                # Please remove completely
+                # print(token["access_token"])
                 # Send success response
                 self.send_response(HTTPStatus.OK)
                 self.send_header("Content-type", "text/html")
