afe57b47f00a27a5bf7917f751c445188ca50e4e
Helmut Hoffer von Ankershoffen <helmuthva@googlemail.com>
2025-10-24 07:51:23 +0200

    chore(tests): PLATFORMN_ENVIRONMENT dependent app versions in test

--- tests/contants_test.py
+++ tests/contants_test.py
@@ -6,7 +6,7 @@ that are shared across different test modules to ensure consistency and easy mai
 
 import os
 
-match os.getenv("AIGNOSTICS_PLATFORM_ENVIRONMENT", "(not set)"):
+match os.getenv("AIGNOSTICS_PLATFORM_ENVIRONMENT"):
     case "production":
         TEST_APPLICATION_ID = "test-app"
         TEST_APPLICATION_VERSION = "0.0.4"
